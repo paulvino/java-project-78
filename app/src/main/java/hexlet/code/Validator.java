e5d36a686d6d9e86a5c003a90449aeb4b492a6f9
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-09-08 07:58:18 +0300

    Test-update (java CI check)

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.StringSchema;
 import hexlet.code.schemas.NumberSchema;
+// for test
 
 public final class Validator {
     public StringSchema string() {
